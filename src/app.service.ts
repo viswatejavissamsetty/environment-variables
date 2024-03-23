2d33b6d409607dbca6782747b57f5051238c64aa
viswatejavissamsetty <viswatejavissamsetty@gmail.com>
2024-03-23 08:33:16 +0530

    added docker setup

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,11 +5,11 @@ export class AppService {
   private readonly logger = new Logger('AppService');
 
   constructor() {
-    this.logger.log('Env Varialbes:', process.env);
+    this.logger.log('Env Varialbes:', JSON.stringify(process.env, null, 4));
   }
 
   getHello() {
-    this.logger.log(process.env);
+    this.logger.log(JSON.stringify(process.env, null, 4));
     return process.env;
   }
 }
